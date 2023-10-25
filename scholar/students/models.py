7455f5a7d41397bc90cd41b5c2232a0df1f75330
sebastianTrej <andrestrejo7777777@gmail.com>
2023-10-24 20:03:09 -0500

    migraciones

--- scholar/students/models.py
+++ scholar/students/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    email= models.CharField(max_length=100)
+    password= models.CharField(max_length=500)
+
+class Notas(models.Model):
+    nota1= models.CharField(max_length=100)
+    nota2= models.CharField(max_length=500)
+    nota3= models.CharField(max_length=500)
+
